fbf6d3630fa7d7d86de115d2e41b59fce96bf7cc
noboevbo <Dennis.Ludl@reutlingen-university.de>
2020-12-16 11:58:46 +0100

    Update get_models.sh
    Updated server URLs

--- get_models.sh
+++ get_models.sh
@@ -4,6 +4,6 @@ mkdir ./ehpi_action_recognition/data
 mkdir ./ehpi_action_recognition/data/models
 cd ./ehpi_action_recognition/data/models
 
-wget https://cogsys.reutlingen-university.de/pub/files/2019_04_ehpi/ehpi_v1.pth
-wget https://cogsys.reutlingen-university.de/pub/files/2019_04_ehpi/yolov3.weights
-wget https://cogsys.reutlingen-university.de/pub/files/2019_04_ehpi/pose_resnet_50_256x192.pth.tar
+wget https://mkiserv114.reutlingen-university.de/pub/files/2019_04_ehpi/ehpi_v1.pth
+wget https://mkiserv114.reutlingen-university.de/pub/files/2019_04_ehpi/yolov3.weights
+wget https://mkiserv114.reutlingen-university.de/pub/files/2019_04_ehpi/pose_resnet_50_256x192.pth.tar
